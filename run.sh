9be858693ce7e106845f377a12ddfe51b37ae4d9
simmarum <mateusz0831@gmail.com>
2018-04-12 21:08:39 +0200

    Add hostfile to script

--- run.sh
+++ run.sh
@@ -4,27 +4,32 @@
 # Second argument is L
 # Third argument is S
 
-INFO_ERROR="\nTry run program with 3 argumetns (N-number of proccesses, L-number of doctors, S-capacity of salon)\n#>bash run.sh 2 3 4"
+INFO_ERROR="\nTry run program with 3 argumetns (N-number of proccesses, L-number of doctors, S-capacity of salon, name_of_hostfile (optional))\n#>bash run.sh 2 3 4\n#>bash run.sh 2 3 4 path/to/hostfile"
 
 if [ -z "$1" ]
   then
     echo "Number of proccesses is missing (N)"
-    echo -e $INFO_ERROR
+    echo "$INFO_ERROR"
     exit 1
 fi
 
 if [ -z "$2" ]
   then
     echo "Number of doctors is missing (L)"
-    echo -e $INFO_ERROR
+    echo "$INFO_ERROR"
     exit 1
 fi
 
 if [ -z "$3" ]
   then
     echo "Capacity of salon is missing (S)"
-    echo -e $INFO_ERROR
+    echo "$INFO_ERROR"
     exit 1
 fi
 
-make && mpirun -np $1 ./contest.out $2 $3
+if [ -n "$4" ]
+  then
+    make && mpirun --hostfile $4 -np $1 ./contest.out $2 $3
+else
+  make && mpirun -np $1 ./contest.out $2 $3
+fi
