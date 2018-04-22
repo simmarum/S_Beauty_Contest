7262302b5d12028d0444b184eacf731a72e47910
simmarum <mateusz0831@gmail.com>
2018-04-22 15:01:35 +0200

    Add tags and improve printf (pretty output)
    - output will be easy to sort by time

---
+++ src/tags.h
@@ -0,0 +1,14 @@
+#ifndef TAGS_H
+#define TAGS_H
+
+#define TAG_END 101  // to end compute and exit process
+
+#define TAG_WANT_DOCTOR 102
+#define TAG_ACK_DOCTOR 103  // acknowledge
+#define TAG_RLS_DOCTOR 104  // release
+
+#define TAG_WANT_SALON 105
+#define TAG_ACK_SALON 106
+#define TAG_RLS_SALON 107
+
+#endif  // TAGS_H
