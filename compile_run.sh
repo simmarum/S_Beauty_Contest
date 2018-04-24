0a1c651087ed32cf7214bfda581df602719612f0
simmarum <mateusz0831@gmail.com>
2018-04-24 20:31:31 +0200

    fix redundant data in script

--- compile_run.sh
+++ compile_run.sh
@@ -1,36 +1,3 @@
 #!/bin/bash
 
-# First argument is N
-# Second argument is L
-# Third argument is S
-# Fourth argument is optional path to hostfile (for mpi)
-
-INFO_ERROR="\nTry run program with 3 argumetns (N-number of proccesses, L-number of doctors, S-capacity of salon, name_of_hostfile (optional))\n#>bash run.sh 2 3 4\n#>bash run.sh 2 3 4 path/to/hostfile"
-
-if [ -z "$1" ]
-  then
-    echo "Number of proccesses is missing (N)"
-    echo "$INFO_ERROR"
-    exit 1
-fi
-
-if [ -z "$2" ]
-  then
-    echo "Number of doctors is missing (L)"
-    echo "$INFO_ERROR"
-    exit 1
-fi
-
-if [ -z "$3" ]
-  then
-    echo "Capacity of salon is missing (S)"
-    echo "$INFO_ERROR"
-    exit 1
-fi
-
-if [ -n "$4" ]
-  then
-    make && mpirun --hostfile $4 -np $1 ./contest $2 $3
-else
-  make && mpirun -np $1 ./contest $2 $3
-fi
+make && sh ./run.sh $1 $2 $3 $4
